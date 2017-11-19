d2e6fdbe2e85b4d237cb1bfa54f44146b783f4dc
xrystalll <mems.lark@gmail.com>
2017-11-19 16:38:46 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ usernameInput.on('keyup', function(e) {
 		user.push(getTxt);
 		usernameInput.val('');
 		$('.initModal').css('display', 'none');
-		set_cookie ( 'username', 'user' );
+		console.log(user);
 	}
 });
 
