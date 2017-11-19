819669a2f7992b4fc3fda0372ae9832253128792
xrystalll <mems.lark@gmail.com>
2017-11-19 19:27:33 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ var user = [];
 					 replace(/<[\/\!]*?[^<>]*?>/gi, '&!2%=_^').
 					 replace(/<style[^>]*?>.*?<\/style>/gi, '#^$!$&1').
 					 replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '@#%&7^').
+					 replace(/[a]*?[\/a]/gi, '<a src="*?">*?</a>').
 					 replace(/#a01/gi, '<img class="emoji" src="src/img/a01.jpg">').
 					 replace(/#a02/gi, '<img class="emoji" src="src/img/a02.jpg">').
 					 replace(/#a03/gi, '<img class="emoji" src="src/img/a03.jpg">').
