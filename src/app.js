8787c4740547a5f276dcc31b12a153d5de367f98
xrystalll <mems.lark@gmail.com>
2017-11-19 14:27:16 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,15 +13,15 @@ var user = [];
 					 replace(/<[\/\!]*?[^<>]*?>/gi, '&!2%=_^').
 					 replace(/<style[^>]*?>.*?<\/style>/gi, '#^$!$&1').
 					 replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '@#%&7^').
-					 replace(/#a01/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/001.jpg">').
-					 replace(/#a02/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/002.jpg">').
-					 replace(/#a03/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/003.jpg">').
-					 replace(/#a04/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/004.jpg">').
-					 replace(/#a05/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/005.jpg">').
-					 replace(/#a06/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/006.jpg">').
-					 replace(/#a07/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/007.jpg">').
-					 replace(/#a08/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/008.jpg">').
-					 replace(/#a09/gi, '<img class="emoji" src="http://v9116084.bget.ru/design/emoji/010.jpg">');
+					 replace(/#a01/gi, '<img class="emoji" src="../img/a01.jpg">').
+					 replace(/#a02/gi, '<img class="emoji" src="../img/a02.jpg">').
+					 replace(/#a03/gi, '<img class="emoji" src="../img/a03.jpg">').
+					 replace(/#a04/gi, '<img class="emoji" src="../img/a04.jpg">').
+					 replace(/#a05/gi, '<img class="emoji" src="../img/a05.jpg">').
+					 replace(/#a06/gi, '<img class="emoji" src="../img/a06.jpg">').
+					 replace(/#a07/gi, '<img class="emoji" src="../img/a07.jpg">').
+					 replace(/#a08/gi, '<img class="emoji" src="../img/a08.jpg">').
+					 replace(/#a09/gi, '<img class="emoji" src="../img/a09.jpg">');
 	    return output;
 	};
 })(jQuery);
