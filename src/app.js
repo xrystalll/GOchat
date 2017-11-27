04a79ab610b0ffd588768417d9eb79e38457ba1a
xrystalll <mems.lark@gmail.com>
2017-11-27 20:40:30 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ input.on('keyup', function(e) {
 });
 
 messages.limitToLast(100).on("child_added", function(snap) {
-	wrap.append('<div class="msb"><div class="cover">X</div><span>' + $.sanitize(snap.val().user) + '</span> <time>' + $.sanitize(snap.val().time) + '</time><div>' + $.sanitize(snap.val().message) + '</div></div>');
+	wrap.append('<div class="msb"><div class="cover">' + $.sanitize(snap.val().user) + '</div><span>' + $.sanitize(snap.val().user) + '</span> <time>' + $.sanitize(snap.val().time) + '</time><div>' + $.sanitize(snap.val().message) + '</div></div>');
 	window.scrollTo(0,document.body.scrollHeight);
 });
 
