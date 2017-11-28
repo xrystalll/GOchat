0d2a02071681ef3dd9b2d79ff217990b6ed25534
xrystalll <mems.lark@gmail.com>
2017-11-28 17:34:02 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -81,7 +81,7 @@ var inTypping = false;
 $('input#emoji_c').keypress(function(){
 	clearInterval(goTyp);
 	if(!inTypping){
-		document.getElementById('status').innerHTML='<img class="load" src="https://gardenlife.com.au/wp-content/themes/garden-life/images/loader.png" alt="load">';
+		document.getElementById('status').innerHTML='<img class="load" src="src/img/loader.gif" alt="load">';
 		inTypping = true;
 		window.scrollTo(0,document.body.scrollHeight);
 	}
