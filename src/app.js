73dcd209079105f3cc5e7d9397ef642f89ae320d
xrystalll <mems.lark@gmail.com>
2017-12-01 18:28:22 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ usernameInput.on('keyup', function(e) {
 		usernameInput.val('');
 		$('.initModal').css('display', 'none');
 		console.log(user);
-		localStorage.setItem("curUsername","user");
+		localStorage.setItem("usernameInput","user");
 	}
 });
 
