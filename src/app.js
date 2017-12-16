b605d4d635cc6ab9f54ac9570491416cbb4f67e4
xrystalll <mems.lark@gmail.com>
2017-12-16 17:00:43 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -79,19 +79,10 @@ for (i = 0; i < elements.length; i++) {
     }
     var getTxt = localStorage.getItem(id)
     user.push(getTxt);
+    $('.initModal').css('display', 'none');
     console.log(user);
   })(elements[i]);
 }
-
-usernameInput.on('keyup', function(e) {
-	if (e.keyCode === 13 && usernameInput.val().length > 0) {
-		var getTxt = usernameInput.val()
-		user.push(getTxt);
-		usernameInput.val('');
-		$('.initModal').css('display', 'none');
-		console.log(user);
-	}
-});
 
 var submit = document.getElementById('send');
 submit.onclick = function () {
