d8f3e97caad76ba0a676874fa7130d87d34fdb69
xrystalll <mems.lark@gmail.com>
2017-12-17 17:29:14 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,51 +11,51 @@ var user = [];
 (function($) {
 	$.sanitize = function(input) {
 		var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '#@$!*6').
-					 replace(/<[\/\!]*?[^<>]*?>/gi, '&!2%=_^').
-					 replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '@#%&7^').
-					 replace(/<style[^>]*?>.*?<\/style>/gi, '#^$!$&1').
-					 replace(/\[img\](.*?)\[\/img\]/gi, '<img class="pic" src="$1">').
-					 replace(/#a01/gi, '<img class="emoji" src="src/img/a01.png">').
-					 replace(/#a02/gi, '<img class="emoji" src="src/img/a02.png">').
-					 replace(/#a03/gi, '<img class="emoji" src="src/img/a03.png">').
-					 replace(/#a04/gi, '<img class="emoji" src="src/img/a04.png">').
-					 replace(/#a05/gi, '<img class="emoji" src="src/img/a05.png">').
-					 replace(/#a06/gi, '<img class="emoji" src="src/img/a06.png">').
-					 replace(/#a07/gi, '<img class="emoji" src="src/img/a07.png">').
-					 replace(/#a08/gi, '<img class="emoji" src="src/img/a08.png">').
-					 replace(/#a09/gi, '<img class="emoji" src="src/img/a09.png">').
-					 replace(/#b01/gi, '<img class="emoji" src="src/img/b01.png">').
-					 replace(/#b02/gi, '<img class="emoji" src="src/img/b02.png">').
-					 replace(/#b03/gi, '<img class="emoji" src="src/img/b03.png">').
-					 replace(/#b04/gi, '<img class="emoji" src="src/img/b04.png">').
-					 replace(/#z01/gi, '<img class="sticker" src="src/img/z01.png">').
-					 replace(/#z02/gi, '<img class="sticker" src="src/img/z02.png">').
-					 replace(/#z03/gi, '<img class="sticker" src="src/img/z03.png">').
-					 replace(/#z04/gi, '<img class="sticker" src="src/img/z04.png">').
-					 replace(/#z05/gi, '<img class="sticker" src="src/img/z05.png">').
-					 replace(/#z06/gi, '<img class="sticker" src="src/img/z06.png">').
-					 replace(/#z07/gi, '<img class="sticker" src="src/img/z07.png">');
-	    return output;
+		replace(/<[\/\!]*?[^<>]*?>/gi, '&!2%=_^').
+		replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '@#%&7^').
+		replace(/<style[^>]*?>.*?<\/style>/gi, '#^$!$&1').
+		replace(/\[img\](.*?)\[\/img\]/gi, '<img class="pic" src="$1">').
+		replace(/#a01/gi, '<img class="emoji" src="src/img/a01.png">').
+		replace(/#a02/gi, '<img class="emoji" src="src/img/a02.png">').
+		replace(/#a03/gi, '<img class="emoji" src="src/img/a03.png">').
+		replace(/#a04/gi, '<img class="emoji" src="src/img/a04.png">').
+		replace(/#a05/gi, '<img class="emoji" src="src/img/a05.png">').
+		replace(/#a06/gi, '<img class="emoji" src="src/img/a06.png">').
+		replace(/#a07/gi, '<img class="emoji" src="src/img/a07.png">').
+		replace(/#a08/gi, '<img class="emoji" src="src/img/a08.png">').
+		replace(/#a09/gi, '<img class="emoji" src="src/img/a09.png">').
+		replace(/#b01/gi, '<img class="emoji" src="src/img/b01.png">').
+		replace(/#b02/gi, '<img class="emoji" src="src/img/b02.png">').
+		replace(/#b03/gi, '<img class="emoji" src="src/img/b03.png">').
+		replace(/#b04/gi, '<img class="emoji" src="src/img/b04.png">').
+		replace(/#z01/gi, '<img class="sticker" src="src/img/z01.png">').
+		replace(/#z02/gi, '<img class="sticker" src="src/img/z02.png">').
+		replace(/#z03/gi, '<img class="sticker" src="src/img/z03.png">').
+		replace(/#z04/gi, '<img class="sticker" src="src/img/z04.png">').
+		replace(/#z05/gi, '<img class="sticker" src="src/img/z05.png">').
+		replace(/#z06/gi, '<img class="sticker" src="src/img/z06.png">').
+		replace(/#z07/gi, '<img class="sticker" src="src/img/z07.png">');
+		return output;
 	};
 })(jQuery);
 
 document.getElementById('emoji_c').onpaste = function (event) {
-  var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-  var blob = null;
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].type.indexOf("image") === 0) {
-      blob = items[i].getAsFile();
-    }
-  }
-  if (blob !== null) {
-    var reader = new FileReader();
-    reader.onload = function(event) {
-      document.getElementById("pastedImage").src = event.target.result;
-      document.getElementById("emoji_c").value = '[img]'+ event.target.result +'[/img]';
+	var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+	var blob = null;
+	for (var i = 0; i < items.length; i++) {
+		if (items[i].type.indexOf("image") === 0) {
+			blob = items[i].getAsFile();
+		}
+	}
+	if (blob !== null) {
+		var reader = new FileReader();
+		reader.onload = function(event) {
+			document.getElementById("pastedImage").src = event.target.result;
+			document.getElementById("emoji_c").value = '[img]'+ event.target.result +'[/img]';
 
-    };
-    reader.readAsDataURL(blob);
-  }
+		};
+		reader.readAsDataURL(blob);
+	}
 }
 
 $( document ).ready(function(){
@@ -70,64 +70,64 @@ document.getElementById("pastedImage").onclick = function(e){
 var elements = document.querySelectorAll('input#username');
 function checkValidity() {};
 for (i = 0; i < elements.length; i++) {
-  (function(element) {
-    var id = element.getAttribute('id');
-    element.value = localStorage.getItem(id);
-    element.oninput = function() {
-      localStorage.setItem(id, element.value);
-      checkValidity();
-    }
-    if (element.value.length > 0) {
-      var getTxt = localStorage.getItem(id);
-      user.push(getTxt);
-      $('.initModal').css('display', 'none');
-      console.log(user);
-    }
-    usernameInput.on('keyup', function(e) {
-      if (e.keyCode === 13 && element.value.length > 0) {
-        var getTxt = localStorage.getItem(id);
-        user.push(getTxt);
-        $('.initModal').css('display', 'none');
-        console.log(user);
-      }
-    });
-  })(elements[i]);
+	(function(element) {
+		var id = element.getAttribute('id');
+		element.value = localStorage.getItem(id);
+		element.oninput = function() {
+			localStorage.setItem(id, element.value);
+			checkValidity();
+		}
+		if (element.value.length > 0) {
+			var getTxt = localStorage.getItem(id);
+			user.push(getTxt);
+			$('.initModal').css('display', 'none');
+			console.log(user);
+		}
+		usernameInput.on('keyup', function(e) {
+			if (e.keyCode === 13 && element.value.length > 0) {
+				var getTxt = localStorage.getItem(id);
+				user.push(getTxt);
+				$('.initModal').css('display', 'none');
+				console.log(user);
+			}
+		});
+	})(elements[i]);
 }
 
 $username = document.getElementById('imgAvatar')
 if(localStorage['image']){
-un.value=localStorage['image']
+	un.value=localStorage['image']
 }else{
-un.value='http://v9116084.bget.ru/files/goload_2584_1512493288.png'
+	un.value='http://v9116084.bget.ru/files/goload_2584_1512493288.png'
 }
 $username.oninput=function(){localStorage['image']=this.value}
 
 var submit = document.getElementById('send');
 submit.onclick = function () {
-    var curUsername = user.join();
-    var imgAvatar = localStorage.getItem(image);
-    var tm = new Date();
-    var nowTime = checkTime(tm.getHours()) + ':' + checkTime(tm.getMinutes());
-    if (input.val().length > 0) {
-        var getTxt = input.val();
-        messages.push({
-		time: nowTime,
-        	user: curUsername,
-		image: imgAvatar,
-        	message: getTxt
-	});
-    input.val('');
-    }
+	var curUsername = user.join();
+	var imgAvatar = localStorage.getItem(image);
+	var tm = new Date();
+	var nowTime = checkTime(tm.getHours()) + ':' + checkTime(tm.getMinutes());
+	if (input.val().length > 0) {
+		var getTxt = input.val();
+		messages.push({
+			time: nowTime,
+			user: curUsername,
+			image: imgAvatar,
+			message: getTxt
+		});
+		input.val('');
+	}
 }
 
 input.on('keyup', function(e) {
 	var curUsername = user.join();
-        var tm = new Date();
-        var nowTime = checkTime(tm.getHours()) + ':' + checkTime(tm.getMinutes());
+	var tm = new Date();
+	var nowTime = checkTime(tm.getHours()) + ':' + checkTime(tm.getMinutes());
 	if (e.keyCode === 13 && input.val().length > 0) {
 		var getTxt = input.val();
 		messages.push({
-		        time: nowTime,
+			time: nowTime,
 			user: curUsername,
 			message: getTxt
 		});
@@ -159,22 +159,22 @@ messages.limitToLast(100).on("child_added", function(snap) {
 });
 
 function emoji_alert() {
-   document.getElementById("emoji_b").classList.toggle("vis");
+	document.getElementById("emoji_b").classList.toggle("vis");
 }
 
 function x () {return;}
 function FocusText() {
-    document.getElementById('emoji_c').focus();
-    document.getElementById('emoji_c').select();
-    return true;
+	document.getElementById('emoji_c').focus();
+	document.getElementById('emoji_c').select();
+	return true;
 }
 function DoSmilie(addSmilie) {
-    var revisedmsgage;
-    var currentmsgage = document.getElementById('emoji_c').value;
-    revisedmsgage = currentmsgage+addSmilie;
-    document.getElementById('emoji_c').value=revisedmsgage;
-    document.getElementById('emoji_c').focus();
-    return;
+	var revisedmsgage;
+	var currentmsgage = document.getElementById('emoji_c').value;
+	revisedmsgage = currentmsgage+addSmilie;
+	document.getElementById('emoji_c').value=revisedmsgage;
+	document.getElementById('emoji_c').focus();
+	return;
 }
 function DoPrompt(action) {
 	var revisedmsgage;
@@ -182,16 +182,16 @@ function DoPrompt(action) {
 }
 
 function es_toggle() {
-   document.getElementById("es_togg").classList.toggle("vis");
+	document.getElementById("es_togg").classList.toggle("vis");
 }
 
 $(document).ready(function() {
-    var menu = $(".menu");
-    var arrow = $(".arrow");
+	var menu = $(".menu");
+	var arrow = $(".arrow");
 
-    $(".toggle").on('click', function() {
-      $.each([menu, arrow], function() {
-          this.toggleClass('out');
-      });
-    });
+	$(".toggle").on('click', function() {
+		$.each([menu, arrow], function() {
+			this.toggleClass('out');
+		});
+	});
 });
