fc39a48cc29722c6767e7b8c91237087f0ad2a7d
xrystalll <mems.lark@gmail.com>
2018-03-24 12:37:01 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
-var fb = new Firebase("https://gochat-ac889.firebaseio.com/");
-var messages = fb.child("messages");
-var btn = $('button');
-var wrap = $('.wrapper');
-var input = $('input.message');
-var usernameInput = $('input.username');
-var userimageInput = $('input.image');
-function checkTime(i) { if (i<10) { i="0" + i; } return i; }
-
-var image = [];
-var user = [];
+var fb = new Firebase("https://gochat-ac889.firebaseio.com/"),
+messages = fb.child("messages"),
+btn = $("button"),
+wrap = $(".wrapper"),
+input = $("input.message"),
+usernameInput = $("input.username"),
+userimageInput = $("input.image"),
+image = [],
+user = [];
+function checkTime(e) { return e<10&&(e="0"+e), e }
 
 (function($) {
 	$.sanitize = function(input) {
@@ -17,6 +16,7 @@ var user = [];
 		replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '@#%&7^').
 		replace(/<style[^>]*?>.*?<\/style>/gi, '#^$!$&1').
 		replace(/\[img\](.*?)\[\/img\]/gi, '<img class="pic" src="$1">').
+		replace(/\[a\](.*?)\[\/a\]/gi, '<a href="$1">$1</a>').
 		replace(/#a01/gi, '<img class="emoji" src="src/img/a01.png">').
 		replace(/#a02/gi, '<img class="emoji" src="src/img/a02.png">').
 		replace(/#a03/gi, '<img class="emoji" src="src/img/a03.png">').
@@ -36,96 +36,75 @@ var user = [];
 		replace(/#z04/gi, '<img class="sticker" src="src/img/z04.png">').
 		replace(/#z05/gi, '<img class="sticker" src="src/img/z05.png">').
 		replace(/#z06/gi, '<img class="sticker" src="src/img/z06.png">').
-		replace(/#z07/gi, '<img class="sticker" src="src/img/z07.png">');
+		replace(/#z07/gi, '<img class="sticker" src="src/img/z07.png">').
+		replace(/#x01/gi, '<img class="sticker" src="src/img/x01.png">').
+		replace(/#x02/gi, '<img class="sticker" src="src/img/x02.png">').
+		replace(/#x03/gi, '<img class="sticker" src="src/img/x03.png">').
+		replace(/#x04/gi, '<img class="sticker" src="src/img/x04.png">');
 		return output;
 	};
 })(jQuery);
 
-document.getElementById('emoji_c').onpaste = function (event) {
-	var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-	var blob = null;
-	for (var i = 0; i < items.length; i++) {
-		if (items[i].type.indexOf("image") === 0) {
-			blob = items[i].getAsFile();
-		}
+document.getElementById("emoji_c").onpaste = function(e) {
+	for (var t=(e.clipboardData||e.originalEvent.clipboardData).items, a=null, n=0; n < t.length; n++)0===t[n].type.indexOf("image")&&(a = t[n].getAsFile());
+	if (null!==a) {
+		var l=new FileReader;
+		l.onload=function(e) {
+			document.getElementById("pastedImage").src = e.target.result, document.getElementById("emoji_c").value="[img]" + e.target.result + "[/img]"
+		}, l.readAsDataURL(a)
 	}
-	if (blob !== null) {
-		var reader = new FileReader();
-		reader.onload = function(event) {
-			document.getElementById("pastedImage").src = event.target.result;
-			document.getElementById("emoji_c").value = '[img]'+ event.target.result +'[/img]';
+};
 
-		};
-		reader.readAsDataURL(blob);
+$(document).ready(function() {
+	$("#pastedImage, #send").click(function() {
+		$("img#pastedImage").removeAttr("src")
+	})
+}),
+document.getElementById("pastedImage").onclick=function(e){
+	document.getElementById("emoji_c").value=""
+};
+
+function checkValidity(){}
+var elements = document.querySelectorAll("input#username");
+for(i=0;i<elements.length;i++)!function(e){
+	var t = e.getAttribute("id");
+	if(e.value = localStorage.getItem(t), e.oninput = function(){
+		localStorage.setItem(t,e.value), checkValidity()
+	}, e.value.length>0){
+		var l = localStorage.getItem(t);
+		user.push(l), $(".initModal").css("display","none"),
+		console.log(user)
 	}
-}
+	usernameInput.on("keyup",function(l){
+		if(13===l.keyCode&&e.value.length>0){
+			var n = localStorage.getItem(t);
+			user.push(n), $(".initModal").css("display","none"),
+			console.log(user)
+		}
+	})
+} (elements[i]);
 
-$( document ).ready(function(){
-	$("#pastedImage, #send").click(function(){
-		$("img#pastedImage").removeAttr("src");
-	});
-});
-document.getElementById("pastedImage").onclick = function(e){
-	document.getElementById("emoji_c").value = "";
-}
-
-var elements = document.querySelectorAll('input#username');
-function checkValidity() {};
-for (i = 0; i < elements.length; i++) {
-	(function(element) {
-		var id = element.getAttribute('id');
-		element.value = localStorage.getItem(id);
-		element.oninput = function() {
-			localStorage.setItem(id, element.value);
-			checkValidity();
+var elements = document.querySelectorAll("input#image");
+for(i=0;i<elements.length;i++)!function(e){
+	var t = e.getAttribute("id");
+	if(e.value = localStorage.getItem(t), e.oninput = function(){
+		localStorage.setItem(t,e.value), checkValidity()
+	}, e.value.length>0){
+		var l = localStorage.getItem(t);
+		image.push(l), $(".chk").css("display","none")
+	}
+	userimageInput.on("keyup",function(l){
+		if(13===l.keyCode&&e.value.length>0){
+			var n = localStorage.getItem(t);
+			image.push(n), $(".chk").css("display","none")
 		}
-		if (element.value.length > 0) {
-			var getTxt = localStorage.getItem(id);
-			user.push(getTxt);
-			$('.initModal').css('display', 'none');
-			console.log(user);
-		}
-		usernameInput.on('keyup', function(e) {
-			if (e.keyCode === 13 && element.value.length > 0) {
-				var getTxt = localStorage.getItem(id);
-				user.push(getTxt);
-				$('.initModal').css('display', 'none');
-				console.log(user);
-			}
-		});
-	})(elements[i]);
-}
-
-var elements = document.querySelectorAll('input#image');
-function checkValidity() {};
-for (i = 0; i < elements.length; i++) {
-	(function(element) {
-		var id = element.getAttribute('id');
-		element.value = localStorage.getItem(id);
-		element.oninput = function() {
-			localStorage.setItem(id, element.value);
-			checkValidity();
-		}
-		if (element.value.length > 0) {
-			var getTxt = localStorage.getItem(id);
-			image.push(getTxt);
-			$('.chk').css('display', 'none');
-		}
-		userimageInput.on('keyup', function(e) {
-			if (e.keyCode === 13 && element.value.length > 0) {
-				var getTxt = localStorage.getItem(id);
-				image.push(getTxt);
-				$('.chk').css('display', 'none');
-			}
-		});
-	})(elements[i]);
-}
+	})
+}(elements[i]);
 
 var submit = document.getElementById('send');
 submit.onclick = function () {
 	var curUsername = user.join();
 	var curUserimage = image.join();
-	console.log('https://github.com/xrystalll');
 	var tm = new Date();
 	var nowTime = checkTime(tm.getHours()) + ':' + checkTime(tm.getMinutes());
 	if (input.val().length > 0) {
@@ -157,32 +136,24 @@ input.on('keyup', function(e) {
 	}
 });
 
-var goTyp = new Object();
-var inTypping = false;
-$('input#emoji_c').keypress(function(){
-	clearInterval(goTyp);
-	if(!inTypping){
-		document.getElementById('status').innerHTML='<img class="load" src="src/img/loader.gif" alt="load">';
-		inTypping = true;
-		window.scrollTo(0,document.body.scrollHeight);
-	}
-	if(inTypping){
-		goTyp = setInterval(function(){
-			document.getElementById('status').innerHTML='';
-			inTypping = false;	
-			clearInterval(goTyp);
-		}, 1000);
-}
-});
+var goTyp = new Object, inTypping = !1;
+$("input#emoji_c").keypress(function() {
+	clearInterval(goTyp),
+	inTypping||(document.getElementById("status").innerHTML='<img class="load" src="src/img/loader.gif" alt="load">',
+		inTypping = !0,window.scrollTo(0,document.body.scrollHeight)), inTypping && (goTyp = setInterval(function() {
+			document.getElementById("status").innerHTML="", inTypping = !1,
+			clearInterval(goTyp)
+		},
+		1e3)
+		)});
 
-messages.limitToLast(100).on("child_added", function(snap) {
-	wrap.append('<div class="msb"><div  onclick="DoSmilie(&#39;' + $.sanitize(snap.val().user) + ',&#39;);" class="cover" style="background-image: url(' + $.sanitize(snap.val().image) + ');"></div><div class="cover_n">' + $.sanitize(snap.val().user) + '</div><span>' + $.sanitize(snap.val().user) + '</span> <time>' + $.sanitize(snap.val().time) + '</time><div>' + $.sanitize(snap.val().message) + '</div></div>');
+messages.limitToLast(69).on("child_added", function(i) {
+	wrap.append('<div class="msb"><div  onclick="DoSmilie(&#39;' + $.sanitize(i.val().user) + ', &#39;);" class="cover" style="background-image: url(' + $.sanitize(i.val().image) + ');"></div><div class="cover_n">' + $.sanitize(i.val().user) + "</div><span>" + $.sanitize(i.val().user) + "</span> <time>" + $.sanitize(i.val().time) + "</time><div>" + $.sanitize(i.val().message) + "</div></div>"),
 	window.scrollTo(0,document.body.scrollHeight);
-	var audio = new Audio();
-	audio.src = 'src/sound/new_in.wav';
-	return function () {
-		audio.play();
-	}();
+	var s = new Audio;
+	return s.src="src/sound/new_in.wav",
+	void s.play()
+	//<div class="empty ic"><div class="empty_words">Сообщений пока нет!</div></div>
 });
 
 function op_set() {
@@ -191,6 +162,11 @@ function op_set() {
 
 function emoji_alert() {
 	document.getElementById("emoji_b").classList.toggle("vis");
+}
+
+function login() {
+	document.getElementById("step1").classList.toggle("oplog");
+	document.getElementById("step2").classList.toggle("cllog");
 }
 
 function x () {return;}
@@ -226,3 +202,57 @@ $(document).ready(function() {
 		});
 	});
 });
+
+
+function gif_box() {
+	document.getElementById("gif_box").classList.toggle("open");
+}
+
+function giphyApiSearch(search){
+	var url;
+	var xmlHttp = new XMLHttpRequest();
+
+	search = search.split(' ').join('+');
+	url = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC&limit=9";
+
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			addPics(xmlHttp.responseText);
+	}
+	xmlHttp.open("GET", url, true);  
+	xmlHttp.send(null);
+}
+
+function addPics(gif) {
+	document.querySelector(".pic-container").innerHTML = "";
+	gif = JSON.parse(gif);
+	if (gif.data.length > 0){
+		for (var x = 0; x < gif.data.length; x++) {
+			var el = document.createElement("img");
+			el.setAttribute("id", "gifpasted")
+			el.setAttribute("src", gif.data[x].images.original.url);
+			el.setAttribute("onclick", "gif_box();DoSmilie('[img]" + gif.data[x].images.original.url + "[/img]');");
+			document.querySelector(".pic-container").appendChild(el);
+		}
+	} else {
+		document.querySelector(".error-container").innerHTML = "Ничего не найдено.<br>Попробуй поискать другое!";
+	}
+}
+
+function startGiphySearch(e){
+	e.preventDefault();
+	var userSearch = document.querySelector("#gif-search-input").value;
+	document.querySelector("#gif-search-input").value = "";
+	document.querySelector(".error-container").innerHTML = "";  
+	giphyApiSearch(userSearch);
+}
+
+document.querySelector("#gif-search-form").addEventListener("submit", function(e){
+	startGiphySearch(e)
+});
+
+document.querySelector("#search-btn").addEventListener("click", function(e){
+	startGiphySearch(e)
+}, false);
+
+giphyApiSearch("cool");
