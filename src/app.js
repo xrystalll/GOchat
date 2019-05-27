16fe6d3ccc5e4d73ebd799030ca78d97587fb394
xrystalll <mems.lark@gmail.com>
2019-05-27 19:38:21 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -165,17 +165,19 @@ messages.limitToLast(69).on("child_added", function(i) {
 	window.scrollTo(0,document.body.scrollHeight);
 });
 
-var s = new Audio;
-$('.control').on('click', function() {
-	$(this).addClass('playing');
-	s.src= $(this).data('src'),
-	s.play(),
-	$(this).find('i').text('pause')
+$(window).on('load', function() {
+	var s = new Audio;
+	$('.control').on('click', function() {
+		$(this).addClass('playing');
+		s.src= $(this).data('src'),
+		s.play(),
+		$(this).find('i').text('pause')
+	});
+	s.onended = function() {
+		$('.playing').find('i').text('play_arrow'),
+		$('.control').removeClass('playing');
+	}
 });
-s.onended = function() {
-	$('.playing').find('i').text('play_arrow'),
-	$('.control').removeClass('playing');
-};
 
 function op_set() {
 	document.getElementById("set-i").classList.toggle("opn")
